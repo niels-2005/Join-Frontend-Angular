abb45510444436518caad3d1c1ae0be4dbd7b15f
niels-2005 <niels.scholz01@web.de>
2023-06-15 20:39:35 +0200

    added popup service

--- src/app/services/addtaskfieldservice.service.ts
+++ src/app/services/addtaskfieldservice.service.ts
@@ -9,9 +9,15 @@ export class AddtaskfieldserviceService {
 
   openTaskField() {
     document.getElementById('add-task-field')?.classList.add('show-task-field');
+    document.getElementById('board-container')?.classList.add('give-opacity');
+    document.getElementById('sitebar')?.classList.add('give-opacity');
+    document.getElementById('kanban-header')?.classList.add('give-opacity');
   }
 
   closeTaskField() {
     document.getElementById('add-task-field')?.classList.remove('show-task-field');
+    document.getElementById('board-container')?.classList.remove('give-opacity');
+    document.getElementById('sitebar')?.classList.remove('give-opacity');
+    document.getElementById('kanban-header')?.classList.remove('give-opacity');
   }
 }
